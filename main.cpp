9087d1f16d530c9e10db0a9db3022f4b23de63c5
Writwick Das <writzx@gmail.com>
2022-06-15 07:53:12 +0000

    setup basic mpi stuff

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
+#include <mpi.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    std::cout << "World Size: " << world_size << "   Rank: " << world_rank << std::endl;
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
+
     return 0;
 }
