722497adc4c444e54e445572acc617a97e83d311
Writwick Das <writzx@gmail.com>
2022-06-19 19:56:19 +0200

    parse input commands and check them for validity

--- main.cpp
+++ main.cpp
@@ -1,12 +1,52 @@
 #include <mpi.h>
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <sstream>
+#include <map>
 
 using namespace std;
 
+void get_func(string op, int operand) {
+
+}
+
 void execute_command(const string &command) {
-    cout << command << endl;
+    istringstream string_stream(command);
+
+    const map<string, pair<void (*)(string, int), vector<string>>> op_map{
+            {"get", {get_func, {"row", "aggr"}}}
+    };
+
+    string op, sub_op, row_str;
+
+    getline(string_stream, op, ' ');
+    getline(string_stream, sub_op, ' ');
+    getline(string_stream, row_str, ' ');
+
+    auto op_element = op_map.find(op);
+    if (op_element == op_map.end()) {
+        // operator is not valid
+        cout << "operator \"" << op << "\" is invalid." << endl;
+        return;
+    }
+
+    const auto allowed_sub_ops = op_element->second.second;
+    if (find(allowed_sub_ops.begin(), allowed_sub_ops.end(), sub_op) == allowed_sub_ops.end()) {
+        // sub-operator is not valid for operator
+        cout << "sub operator \"" << sub_op << "\" is invalid for operator \"" << op << "\"." << endl;
+        return;
+    }
+
+    stringstream row_stream(row_str);
+    int row(0);
+    row_stream >> row;
+
+    if (row_stream.fail()) {
+        cout << "failed to parse row index." << endl;
+        return;
+    }
 }
 
 vector<string> read_commands(const string &path) {
