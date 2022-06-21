cf5d468ed6cc6684493e1e4b936430af4547a953
Writwick Das <writzx@gmail.com>
2022-06-21 17:40:17 +0200

    added executor class

---
+++ Executor.cpp
@@ -0,0 +1,59 @@
+//
+// Created by arik on 21/06/2022.
+//
+
+#include <sstream>
+#include <algorithm>
+
+#include "Executor.h"
+
+using namespace std;
+
+void Executor::get_func(Executor * executor, string sub_op, int row) {
+    int rows_per_rank = executor->total_rows / executor->total_ranks;
+    int rank = row / rows_per_rank;
+
+    cout << "Rank: " << rank << endl;
+}
+
+void Executor::execute_command(const string &command) {
+    istringstream string_stream(command);
+
+    string op, sub_op, row_str;
+
+    // split the command into op, sub op and row index strings
+    getline(string_stream, op, ' ');
+    getline(string_stream, sub_op, ' ');
+    getline(string_stream, row_str, ' ');
+
+    // try to find operator in operator map keys
+    auto op_element = op_map.find(op);
+    if (op_element == op_map.end()) {
+        // operator is not valid
+        cout << "Error: operator \"" << op << "\" is invalid." << endl;
+        return;
+    }
+
+    const auto op_func = op_element->second.first;
+    const auto allowed_sub_ops = op_element->second.second;
+
+    // try to find sub operator in allowed sub operators list
+    if (find(allowed_sub_ops.begin(), allowed_sub_ops.end(), sub_op) == allowed_sub_ops.end()) {
+        // sub-operator is not valid for operator
+        cout << "Error: sub operator \"" << sub_op << "\" is invalid for operator \"" << op << "\"." << endl;
+        return;
+    }
+
+    // try to parse the row value into integer
+    stringstream row_stream(row_str);
+    int row(0);
+    row_stream >> row;
+
+    if (row_stream.fail()) {
+        cout << "Error: failed to parse row index." << endl;
+        return;
+    }
+
+    // call the op_func to execute the command
+    op_func(this, sub_op, row);
+}
--- main.cpp
+++ main.cpp
@@ -1,72 +1,12 @@
 #include <mpi.h>
-#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <sstream>
-#include <map>
+
+#include "Executor.h"
 
 using namespace std;
-
-// TODO take this as input somehow
-int big_N = 300;
-
-// TODO define as a class field
-int total_rank; // #n
-
-void get_func(string sub_op, int row) {
-    int rows_per_rank = big_N / total_rank;
-    int rank = row / rows_per_rank;
-
-    cout << "Rank: " << rank << endl;
-}
-
-// holds the operators, functions and sub operators
-const map<string, pair<void (*)(string, int), vector<string>>> op_map{
-        {"get", {get_func, {"row", "aggr"}}}
-};
-
-void execute_command(const string &command) {
-    istringstream string_stream(command);
-
-    string op, sub_op, row_str;
-
-    // split the command into op, sub op and row index strings
-    getline(string_stream, op, ' ');
-    getline(string_stream, sub_op, ' ');
-    getline(string_stream, row_str, ' ');
-
-    // try to find operator in operator map keys
-    auto op_element = op_map.find(op);
-    if (op_element == op_map.end()) {
-        // operator is not valid
-        cout << "operator \"" << op << "\" is invalid." << endl;
-        return;
-    }
-
-    const auto op_func = op_element->second.first;
-    const auto allowed_sub_ops = op_element->second.second;
-
-    // try to find sub operator in allowed sub operators list
-    if (find(allowed_sub_ops.begin(), allowed_sub_ops.end(), sub_op) == allowed_sub_ops.end()) {
-        // sub-operator is not valid for operator
-        cout << "sub operator \"" << sub_op << "\" is invalid for operator \"" << op << "\"." << endl;
-        return;
-    }
-
-    // try to parse the row value into integer
-    stringstream row_stream(row_str);
-    int row(0);
-    row_stream >> row;
-
-    if (row_stream.fail()) {
-        cout << "failed to parse row index." << endl;
-        return;
-    }
-
-    // call the op_func to execute the command
-    op_func(sub_op, row);
-}
 
 vector<string> read_commands(const string &path) {
     ifstream file_stream(path);
@@ -81,25 +21,42 @@ vector<string> read_commands(const string &path) {
 }
 
 int main(int argc, char **argv) {
+    if (argc < 2) {
+        cout << "Error: total number of rows not specified." << endl;
+        return 0;
+    }
     MPI_Init(&argc, &argv);
 
+    int total_rank;
     MPI_Comm_size(MPI_COMM_WORLD, &total_rank);
 
     int current_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &current_rank);
 
+    // try to parse N from command line
+    stringstream bigN_str(argv[1]);
+    int bigN(0);
+    bigN_str >> bigN;
+
+    if (bigN_str.fail()) {
+        cout << "Error: invalid value for N." << endl;
+        return 0;
+    }
+
+    Executor exec(bigN, total_rank);
+
     if (current_rank == 0) {
-        if (argc < 2) {
+        if (argc < 3) {
             string command;
             while (getline(cin, command)) {
                 if (command == "exit")
                     break;
-                execute_command(command);
+                exec.execute_command(command);
             }
         } else {
-            auto commands = read_commands(argv[1]);
+            auto commands = read_commands(argv[2]);
             for (const string &command: commands) {
-                execute_command(command);
+                exec.execute_command(command);
             }
         }
     }
